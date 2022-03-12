b030f1bb7ef8cc5a3db1fde1c88c43665f270f51
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2022-03-12 12:20:53 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  // state
+  const [listOfCharacters, setListOfCharacters] = useState([]);
+
+  // fetch
+  const fetchData = async (url) => {
+    const rawResponse = await fetch(url);
+    const response = await rawResponse.json();
+
+    return response;
+  };
+
+  useEffect(() => {
+    const getCharacters = async () => {
+      const characters = [];
+
+      for (let i = 1; i <= 10; i++) {
+        let characterData = {
+          characterName: "",
+          characterHomeworld: "",
+        };
+
+        const character = await fetchData(`https://swapi.dev/api/people/${i}/`);
+        const homeworld = await fetchData(character.homeworld);
+
+        characterData.characterName = character.name;
+        characterData.characterHomeworld = homeworld.name;
+
+        characters.push(characterData);
+      }
+
+      setListOfCharacters(characters);
+    };
+
+    getCharacters();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {listOfCharacters.map((character, index) => {
+        const { characterName, characterHomeworld } = character;
+
+        return (
+          <li key={index}>
+            {`${characterName} is from ${characterHomeworld}`}
+          </li>
+        );
+      })}
+    </ul>
   );
 }
 
